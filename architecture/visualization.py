c2a564c1e6d1e5e880b853fb86537516259ee52b
ambamichal <59073279+ambamichal@users.noreply.github.com>
2021-08-29 12:02:18 +0200

    Add files via upload

---
+++ architecture/visualization.py
@@ -0,0 +1,86 @@
+from tensorflow.keras.models import load_model
+
+model = load_model('output/model_28_08_2021_20_44_convnet2_lr0.0001_final.hdf5')
+model.summary()
+
+img_path = 'C:/Users/AmbruszkiewM/PycharmProjects/klasyfikator_tf/images/test/surfboard/44.channel-islands-fish-surfboard-futures-blue_a.jpg'
+
+from tensorflow.keras.preprocessing import image
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras import models
+
+img = image.load_img(img_path, target_size=(224, 224))
+img_tensor = image.img_to_array(img)
+img_tensor = np.expand_dims(img_tensor, axis=0)
+img_tensor /= 255
+print(img_tensor.shape) #kształt tensora
+
+
+#plt.imshow(img_tensor[0])
+#plt.show()
+
+
+layer_outputs = [layer.output for layer in model.layers[:8]]
+activation_model = models.Model(inputs=model.input, outputs=layer_outputs)
+activations = activation_model.predict(img_tensor)
+
+first_layer_activation = activations[0]
+print(first_layer_activation.shape)
+
+plt.matshow(first_layer_activation[0, :, :, 1], cmap='viridis')
+plt.show()
+plt.matshow(first_layer_activation[0, :, :, 2], cmap='viridis')
+plt.show()
+plt.matshow(first_layer_activation[0, :, :, 3], cmap='viridis')
+plt.show()
+plt.matshow(first_layer_activation[0, :, :, 4], cmap='viridis')
+plt.show()
+plt.matshow(first_layer_activation[0, :, :, 5], cmap='viridis')
+plt.show()
+plt.matshow(first_layer_activation[0, :, :, 6], cmap='viridis')
+plt.show()
+
+
+#wizualizacja wszystkich kanałów
+
+layer_names = []
+for layer in model.layers[:8]:
+    layer_names.append(layer.name)
+
+images_per_row = 16
+
+for layer_name, layer_activation in zip(layer_names, activations):
+
+    n_features = layer_activation.shape[-1]
+
+    size = layer_activation.shape[1]
+
+    n_cols = n_features // images_per_row
+
+    display_grid = np.zeros((size * n_cols, images_per_row * size))
+
+    for col in range(n_cols):
+        for row in range(images_per_row):
+            channel_image = layer_activation[0, :, :, col * images_per_row + row]
+            channel_image -= channel_image.mean()
+            np.seterr(divide='ignore', invalid='ignore')
+            channel_image /= np.std(channel_image)
+            channel_image *= 64
+            channel_image += 128
+            channel_image = np.clip(channel_image, 0, 255).astype('uint8')
+            display_grid[col * size : (col + 1) * size, row * size : (row + 1) * size] = channel_image
+
+    scale = 1. /size
+    x = np.isfinite (scale * display_grid[1]).all()
+    y = np.isfinite (scale * display_grid.shape[0]).all()
+    plt.figure(figsize =(x, y))
+    plt.title(layer_name)
+    plt.grid(False)
+    plt.imshow(display_grid, aspect='auto', cmap='viridis')
+plt.show()
+
+
+
+
+
